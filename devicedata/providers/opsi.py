0b4c4589545951062210dc56bc36da8850d8a3a0
Tobias Bengfort <bengfort@mpib-berlin.mpg.de>
2021-01-28 09:31:47 +0100

    do not even import opsi/puppet if not activated
    fixes "AttributeError module 'Lagerregal.settings' has no attribute 'PUPPETDB_SETTINGS'"

--- devicedata/providers/opsi.py
+++ devicedata/providers/opsi.py
@@ -91,11 +91,10 @@ class OpsiProvider(BaseProvider):
     __connection = None
 
     def __init__(self):
-        if settings.USE_OPSI:
-            __connection = OpsiConnection(settings.OPSI_SETTINGS["host"] + ":" + settings.OPSI_SETTINGS["port"],
-                                          username=settings.OPSI_SETTINGS["username"],
-                                          password=settings.OPSI_SETTINGS["password"],
-                                          legal_methods_path="devicedata/providers/rpc_methods.txt")
+        __connection = OpsiConnection(settings.OPSI_SETTINGS["host"] + ":" + settings.OPSI_SETTINGS["port"],
+                                      username=settings.OPSI_SETTINGS["username"],
+                                      password=settings.OPSI_SETTINGS["password"],
+                                      legal_methods_path="devicedata/providers/rpc_methods.txt")
 
     def __get_host(self, device):
         host = None
